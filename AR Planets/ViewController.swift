cb23d7080fa3096d09fbb008255c676d70f2ae37
R. Kukuh <rkukuh@gmail.com>
2018-01-19 20:01:59 +0700

    Define method to create planet objects

--- AR Planets/ViewController.swift
+++ AR Planets/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         sceneView.delegate = self
         
-        // call create planet function here
+        createPlanets()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -69,5 +69,42 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         planNode.geometry = plane
         
         return planNode
+    }
+    
+    func createPlanets() {
+        
+        let sunObject = SCNSphere(radius: 0.9)
+        let moonObject = SCNSphere(radius: 0.1)
+        let earthObject = SCNSphere(radius: 0.2)
+        
+        let sunMaterial = SCNMaterial()
+        let moonMaterial = SCNMaterial()
+        let earthMaterial = SCNMaterial()
+        
+        sunMaterial.diffuse.contents = UIImage(named: "art.scnassets/2k/sun.jpg")
+        moonMaterial.diffuse.contents = UIImage(named: "art.scnassets/2k/moon.jpg")
+        earthMaterial.diffuse.contents = UIImage(named: "art.scnassets/2k/earth.jpg")
+        
+        sunObject.materials = [sunMaterial]
+        moonObject.materials = [moonMaterial]
+        earthObject.materials = [earthMaterial]
+        
+        let sunNode = SCNNode()
+        let moonNode = SCNNode()
+        let earthNode = SCNNode()
+        
+        sunNode.position = SCNVector3(x: 0, y: 0, z: -2)
+        earthNode.position = SCNVector3(x: 0.5, y: 0.4, z: -1)
+        moonNode.position = SCNVector3(x: 0.6, y: 0.3, z: -0.5)
+        
+        sunNode.geometry = sunObject
+        moonNode.geometry = moonObject
+        earthNode.geometry = earthObject
+        
+        sceneView.scene.rootNode.addChildNode(sunNode)
+        sceneView.scene.rootNode.addChildNode(moonNode)
+        sceneView.scene.rootNode.addChildNode(earthNode)
+        
+        sceneView.autoenablesDefaultLighting = true
     }
 }
